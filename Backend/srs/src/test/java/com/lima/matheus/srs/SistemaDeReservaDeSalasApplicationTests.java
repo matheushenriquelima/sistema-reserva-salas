2ef3df0a3653cbd7075c9045f660795ca1250573
matheushenriquelima <mateus.matheus.lima@gmail.com>
2021-01-20 17:28:53 -0300

    inicializando projeto

---
+++ Backend/srs/src/test/java/com/lima/matheus/srs/SistemaDeReservaDeSalasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lima.matheus.srs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeReservaDeSalasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
